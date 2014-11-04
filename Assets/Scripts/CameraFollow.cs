ae4abc8be8cfeacce7df814a0d196ba84a0f45cf
karavias <dim1988g@hotmail.com>
2014-11-04 12:31:09 +0100

    working on the character control
    Adding a sprite character and implementing basic functionalities like,
    move, jump, hit. Also making the 2d sprites change their order in layer
    depending on their y position.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("MyPlayer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (player.transform.position + " - " + player.name);
+		transform.position = new Vector3 (player.transform.position.x,
+		                                  transform.position.y,
+		                                  transform.position.z);
+	}
+}
