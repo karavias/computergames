92779d5aedb3b4e5273ce65a86947b2209da63bb
Jellezilla <doobie@doobies-MacBook-Pro.local>
2014-11-11 12:00:25 +0100

    AI startet
    Created “scene02” and started developing the AI, which is just a box as
    of right now :)

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class EnemyMove : MonoBehaviour {
 
+
+	private GameObject target;
+	public float moveSpeed;
+	private float posY;
+	private float posX; 
 	// Use this for initialization
 	void Start () {
-	
+		target = GameObject.FindGameObjectWithTag("MyPlayer");
+		moveSpeed = 0.03F;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float distance = Vector2.Distance (transform.position, target.transform.position);
+		if(distance < 5.0F)
+		{
+			enemyMoveMethod();
+		}
+
+	}
+
+
+	void enemyMoveMethod() {
+		if(transform.position.y > target.transform.position.y+0.1F) {
+			posY = transform.position.y - (moveSpeed/2);
+			//Debug.Log ("Move down");
+		}
+		else if(transform.position.y < target.transform.position.y-0.1F) {
+			posY = transform.position.y + (moveSpeed/2);
+			//Debug.Log ("Move up");
+		}
+		
+		if(transform.position.x > target.transform.position.x + 1) {
+			posX = transform.position.x - moveSpeed;
+		}
+		else if(transform.position.x < target.transform.position.x - 1) {
+			posX = transform.position.x + moveSpeed;
+		}
+		transform.position = new Vector3(posX, posY, transform.position.z);
 	
 	}
 }
